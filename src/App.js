3785bde5ed407323615e9008c30f1a9e76468ec0
Shyam2397 <shyamsundarr10@gmail.com>
2023-06-19 18:06:06 +0530

    Routes set to Created pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import StudentsList from './pages/StudentsList';
 import TeachersList from './pages/TeachersList';
 import InterviewNotes from './pages/InterviewNotes';
 import Profile from './pages/Profile';
+import Nopage from './pages/Nopage';
 
 
 function App() {
@@ -12,10 +13,11 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path='/' element={<DashBoard/>}/>
-        <Route exact path='/students/all' element={<StudentsList/>}/>
-        <Route exact path='/teachers/all' element={<TeachersList/>}/>
-        <Route exact path='/interview/notes' element={<InterviewNotes/>}/>
-        <Route exact path='/profile' element={<Profile/>}/>
+        <Route  path='/students/all' element={<StudentsList/>}/>
+        <Route  path='/teachers/all' element={<TeachersList/>}/>
+        <Route  path='/interview/notes' element={<InterviewNotes/>}/>
+        <Route  path='/profile' element={<Profile/>}/>
+        <Route  path='*' element={<Nopage/>}/>
       </Routes>
     </div>
   );
