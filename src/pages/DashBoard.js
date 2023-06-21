ab77deaa084f82062d23fca117a9adb49881a4be
Shyam sundar R <shyamsundarr10@gmail.com>
2023-06-21 12:59:28 +0530

    dash board update

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -10,7 +10,7 @@ export default function DashBoard() {
         <div className="text-center h-10 font-bold text-4xl">
           <h1 className=" m-5">Welcome</h1>
         </div>
-        <div className="grid grid-cols-5 mt-10">
+        <div className="flex flex-row flex-wrap gap-5 justify-evenly mt-10">
           <div className="card bg-white rounded-3xl w-52 p-3 place-items-center">
             <h1 className="font-bold">Students</h1>
             <button
