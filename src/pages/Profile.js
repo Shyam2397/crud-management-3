d64b280b44ff98ddf9c9502b0369befaa20f4eb0
Shyam sundar R <shyamsundarr10@gmail.com>
2023-06-27 20:14:24 +0530

    login button is added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import NavSideBar from "../components/NavSideBar";
 
 export default function Profile() {
+  const navigate = useNavigate()
   return (
     <NavSideBar>
-      <div>Profile</div>
+      <div>Profile
+      <div className="card bg-white rounded-3xl w-52 p-3 place-items-center">
+            <h1 className="font-bold">login</h1>
+            <button 
+              className="btn btn-primary rounded-full mt-3"
+              onClick={() => navigate("/login")}
+              >
+              LoginPage
+            </button>
+          </div>
+      </div>
     </NavSideBar>
   );
 }
