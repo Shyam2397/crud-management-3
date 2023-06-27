ee5df0c3c33b75c1f70828ac6cd62fdc78bf7127
Shyam sundar R <shyamsundarr10@gmail.com>
2023-06-27 21:15:51 +0530

    remove a login button

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,17 +5,7 @@ export default function Profile() {
   const navigate = useNavigate()
   return (
     <NavSideBar>
-      <div>Profile
-      <div className="card bg-white rounded-3xl w-52 p-3 place-items-center">
-            <h1 className="font-bold">login</h1>
-            <button 
-              className="btn btn-primary rounded-full mt-3"
-              onClick={() => navigate("/login")}
-              >
-              LoginPage
-            </button>
-          </div>
-      </div>
+      <div>Profile</div>
     </NavSideBar>
   );
 }
