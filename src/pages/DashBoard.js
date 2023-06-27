6db41c298b13da280431754086f1f96aa6e0b0f0
Shyam sundar R <shyamsundarr10@gmail.com>
2023-06-27 21:15:38 +0530

    proviede a login button

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -29,6 +29,15 @@ export default function DashBoard() {
               Add more teachers
             </button>
           </div>
+          <div className="card bg-white rounded-3xl w-52 p-3 place-items-center">
+            <h1 className="font-bold">login</h1>
+            <button 
+              className="btn btn-primary rounded-full mt-3"
+              onClick={() => navigate("/login")}
+              >
+              LoginPage
+            </button>
+          </div>
         </div>
       </div>
     </NavSideBar>
